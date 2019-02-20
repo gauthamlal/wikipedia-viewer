fad4f5999a67e426201b6bf04217bfcf5da79b23
Gautham Lal <gautham1995@gmail.com>
2019-02-21 00:14:52 +0530

    Added redux and stored the search results in the store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+
+import store from "./store";
+import Landing from "./components/Landing";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Wikipedia Viewer</h1>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>Wikipedia Viewer</h1>
+          <Landing />
+        </div>
+      </Provider>
     );
   }
 }
